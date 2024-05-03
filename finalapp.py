27423ad0c7cbdf28d51891ac6cf94fae49601897
suellenf762 <65107104+suellenf762@users.noreply.github.com>
2024-05-03 20:34:08 +1000

    Update finalapp.py

--- finalapp.py
+++ finalapp.py
@@ -79,7 +79,7 @@ def display_page(page_num):
         st.write("The early signs and symptoms of Type 2 Diabetes can include:")
 
         signs = [
-            "Frequent urination :toilet:", "Increased thirst", "Fatigue", "Blurred vision",
+            "Frequent urination :toilet:", "Increased thirst :potable_water:", "Fatigue", "Blurred vision",
             "Slow healing of cuts and wounds", "Tingling numbness or pain in hands or feet",
             "Patches of darker skin", "Itching and yeast infections"
         ]
