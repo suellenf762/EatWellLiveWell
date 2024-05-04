cc8f7300c73c04acac1549969d1538e194b59f43
suellenf762 <65107104+suellenf762@users.noreply.github.com>
2024-05-04 20:43:07 +1000

    Update finalapp.py

--- finalapp.py
+++ finalapp.py
@@ -140,6 +140,12 @@ def display_page(page_num):
         serves_per_day_refgrain= float(serves[:1])
         serves_per_day_refgrain
 
+        st.image("whgrainserve.png",caption="Serving Size Whole Grains")
+        serves = st.select_slider(f"How many serves of whole grains per day?", options=["0-1", "2-3", "4 or more"],
+                                  help="hep")
+        serves_per_day_whgrain= float(serves[:1])
+        serves_per_day_whgrain
+
         st.image("prmeatserve.png",caption="Serving Size Processed Meats")
         serves = st.select_slider(f"How many serves of processed meat per day?", options=["0-1", "2-3", "4 or more"],
                                   help="hep")
