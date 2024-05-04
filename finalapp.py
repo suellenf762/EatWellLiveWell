334d08327e8864cbf68194e00c62c6b3d5b45fea
suellenf762 <65107104+suellenf762@users.noreply.github.com>
2024-05-04 20:00:24 +1000

    Update finalapp.py

--- finalapp.py
+++ finalapp.py
@@ -159,7 +159,7 @@ def display_page(page_num):
         serves_per_day_unprmeat
 
         st.image("swbeverageserve.png",caption="Serving Size Sweetened Beverage")    
-        serves = st.select_slider(f"How many serves ofSweetened Beverage per day?", options=["0-1", "2-3", "4 or more"],
+        serves = st.select_slider(f"How many serves of Sweetened Beverage per day?", options=["0-1", "2-3", "4 or more"],
                                   help="hep")
         serves_per_day_swbeverage= float(serves[:1])
         serves_per_day_swbeverage
