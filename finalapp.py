9645f98b4cc5d22a1d5c7d05b97e4acfacdeaa60
suellenf762 <65107104+suellenf762@users.noreply.github.com>
2024-05-05 10:11:13 +1000

    Update finalapp.py

--- finalapp.py
+++ finalapp.py
@@ -135,7 +135,7 @@ def display_page(page_num):
         serves_per_day_dairy
 
         st.image("refgrainserve.png", caption="Serving Size Refined Grains")
-        serves = st.select_slider(f"How many serves of refined grains per day?", options=["0-1", "2-3", "4 or more"],
+        serves = st.select_slider(f"How many serves of refined grains per day?", options=["0-1", "2-3","4-5", "6 or more"],
                                   help="hep")
         serves_per_day_refgrain = float(serves[:1])*50
         serves_per_day_refgrain
